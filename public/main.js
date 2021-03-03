0771c0ba65f59bb341ca3152647763a5466d7156
Christopher de Oliveira <christopherforwork@gmail.com>
2021-03-03 17:18:04 -0400

    ✨ (Username): Started to add events to change username

--- public/main.js
+++ public/main.js
@@ -1,1 +1,24 @@
 const socket = io();
+
+let username = '';
+
+let userList = [];
+
+let loginPage = document.querySelector('#loginPage');
+let chatPage = document.querySelector('#chatPage');
+let loginInput = document.querySelector('#loginNameInput');
+let textInput = document.querySelector('#chatTextInput');
+
+loginPage.style.display = 'flex';
+chatPage.style.display = 'none';
+
+loginInput.addEventListener('keyup', event => {
+  if(event.keyCode === 13){
+    let name = loginInput.value.trim();
+    
+    if(name !== '') {
+      username = name;
+      document.title = `${username} - Chat`;
+    };
+  };
+});
