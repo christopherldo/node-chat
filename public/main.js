11884d5f018d85acf8572fef58f92b51a085e00b
Christopher de Oliveira <christopherforwork@gmail.com>
2021-03-04 12:49:31 -0400

    🔖 (0.0.1): Released first version of the project

--- public/main.js
+++ public/main.js
@@ -12,13 +12,130 @@ let textInput = document.querySelector('#chatTextInput');
 loginPage.style.display = 'flex';
 chatPage.style.display = 'none';
 
+loginInput.focus();
+
+const renderUserList = () => {
+  let ul = document.querySelector('.userList');
+  ul.innerHTML = '';
+
+  userList.forEach(item => {
+    ul.innerHTML += `<li>${item}</li>`
+  });
+};
+
+const addMessage = (type, user, msg) => {
+  let ul = document.querySelector('.chatList');
+
+  switch (type) {
+    case 'status':
+      ul.innerHTML += `<li class="m-status">${msg}</li>`;
+      break;
+    case 'msg':
+      if (username === user) {
+        ul.innerHTML += `<li class="m-txt"><span class="me">${user}</span> ${msg}</li>`;
+      } else {
+        ul.innerHTML += `<li class="m-txt"><span>${user}</span> ${msg}</li>`;
+      };
+      break;
+  };
+
+  ul.scrollTop = ul.scrollHeight;
+};
+
+const disableMessage = message => {
+  textInput.setAttribute('disabled', '');
+  textInput.setAttribute('placeholder', message);
+};
+
+const enableMessage = message => {
+  textInput.removeAttribute('disabled');
+  textInput.setAttribute('placeholder', message);
+};
+
+disableMessage('Conexão não estabelecida...');
+
 loginInput.addEventListener('keyup', event => {
-  if(event.keyCode === 13){
+  if (event.keyCode === 13) {
     let name = loginInput.value.trim();
-    
-    if(name !== '') {
+
+    if (name) {
       username = name;
       document.title = `${username} - Chat`;
+
+      socket.emit('join-request', username);
     };
   };
+});
+
+textInput.addEventListener('keyup', event => {
+  if (event.keyCode === 13) {
+    let txt = textInput.value.trim();
+    textInput.value = '';
+
+    if (txt) {
+      socket.emit('send-msg', txt);
+    };
+  };
+});
+
+socket.on('user-ok', data => {
+  loginPage.style.display = 'none';
+  chatPage.style.display = 'flex';
+
+  enableMessage('Digite uma mensagem...');
+
+  textInput.focus();
+
+  addMessage('status', null, 'Conectado');
+
+  userList = data;
+  renderUserList();
+});
+
+socket.on('list-update', data => {
+  if (data.joined) {
+    addMessage('status', null, `${data.joined} entrou no chat!`);
+  };
+
+  if (data.left) {
+    addMessage('status', null, `${data.left} saiu do chat`);
+  };
+
+  userList = data.list;
+
+  renderUserList();
+});
+
+socket.on('show-msg', data => {
+  addMessage('msg', data.username, data.message);
+});
+
+socket.on('disconnect', () => {
+  addMessage('status', null, 'Você foi desconectado!');
+
+  disableMessage('Você foi desconectado...');
+
+  userList = [];
+
+  renderUserList();
+});
+
+socket.io.on('reconnect_attempt', () => {
+  addMessage('status', null, 'Tentando reconexão...');
+
+  disableMessage('Tentando reconexão...');
+});
+
+socket.io.on('reconnect', () => {
+  addMessage('status', null, 'Reconectado!');
+
+  disableMessage('Conexão reestabelecida...');
+
+  if(username){
+    socket.emit('join-request', username);
+  };
+});
+
+socket.on('user-error', message => {
+  alert(message);
 });
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const http = require('http');
 const socketIO = require('socket.io');
+const sanitizer = require('sanitize-html');
 
 const app = express();
 const server = http.createServer(app);
@@ -11,6 +12,75 @@ server.listen(3000);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+let connectedUsers = [];
+
+const sanitizeHTML = (string) => {
+  sanitizedString = sanitizer(string, {
+    allowedTags: [],
+    allowedAttributes: {},
+    disallowedTagsMode: 'escape',
+  });
+
+  return sanitizedString;
+};
+
 io.on('connection', socket => {
   console.log(`Conexão detectada...`);
+
+  socket.on('join-request', (username) => {
+    if (username.length <= 0 || username.length >= 16) {
+      socket.emit('user-error', 'O usuário informado precisa conter de 0 a 16 caracteres!');
+      return;
+    };
+
+    username = sanitizeHTML(username);
+
+    if (connectedUsers.includes(username) === false) {
+      socket.username = username;
+
+      connectedUsers.push(username);
+
+      console.log(`Usuário conectado: ${username}`);
+      console.log(`Total de usuários: ${connectedUsers.length}`)
+
+      socket.emit('user-ok', connectedUsers);
+
+      socket.broadcast.emit('list-update', {
+        joined: username,
+        list: connectedUsers,
+      });
+    } else {
+      socket.emit('user-error', 'Usuário informado já está conectado!');
+    };
+  });
+
+  socket.on('disconnect', () => {
+    connectedUsers = connectedUsers.filter(user => user !== socket.username);
+
+    console.log(`Usuário desconectado: ${socket.username}`);
+    console.log(`Total de usuários: ${connectedUsers.length}`)
+
+    socket.broadcast.emit('list-update', {
+      left: socket.username,
+      list: connectedUsers,
+    });
+  });
+
+  socket.on('send-msg', txt => {
+    if (socket.username === undefined) {
+      return;
+    };
+
+    txt = sanitizeHTML(txt);
+
+    if (txt) {
+      let obj = {
+        username: socket.username,
+        message: txt,
+      };
+
+      socket.emit('show-msg', obj);
+      socket.broadcast.emit('show-msg', obj);
+    };
+  });
 });
